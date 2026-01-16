ea52235683013b792fac63eb15cb96077d2b152e
AliceAliciaBB <reimari.ge.mu.2@gmail.com>
2026-01-16 23:42:03 +0900

    最新

--- ビルド.bat
+++ ビルド.bat
@@ -7,6 +7,7 @@ taskkill /F /IM "EterPix VRC Uploader.exe" 2>nul
 
 REM ビルドフォルダを削除
 rmdir /s /q "%~dp0build" 2>nul
+rmdir /s /q "%~dp0dist" 2>nul
 
 REM PyInstallerでビルド
 pyinstaller "EterPix VRC Uploader.spec" --noconfirm
